5997106a2491fcd7bba3694c12e7c395a7d43cec
John Trammell <johntrammell@gmail.com>
2008-09-12 15:36:23 -0500

    adding median function

--- chase/lib/chase.scm
+++ chase/lib/chase.scm
@@ -2,6 +2,7 @@
 (export erf)
 (export erfc)
 (export Phi)
+(export avg)
 
 (load-extension "cygguile-erf" "init_erf")   ; defines 'erf' function
 
@@ -14,4 +15,25 @@
   (lambda (x)
     (* 0.5
        (+ 1.0 (erf (/ x (sqrt 2)))))))
+
+(define avg
+  (lambda (lst)
+    (/ (apply + lst) (length lst))))
+
+; (define median
+;   (lambda (vec) (    )))
+
+; (define select
+;   (lambda (k . vec)))
+
+; function select(list[1..n], k)
+;     for i from 1 to k
+;         minIndex = i
+;         minValue = list[i]
+;         for j from i+1 to n
+;             if list[j] < minValue
+;                 minIndex = j
+;                 minValue = list[j]
+;         swap list[i] and list[minIndex]
+;     return list[k]
 
