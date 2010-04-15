8390df5c0cd4bd48a413f3f6bdfeac834201bae8
John Trammell <johntrammell@gmail.com>
2010-04-15 13:31:11 -0500

    fixed factorial loading

--- bev/lib/bev/permute.scm
+++ bev/lib/bev/permute.scm
@@ -1,7 +1,9 @@
 ; P(n,r) = n! / (n-r)!
+
+(load "factorial.scm")
 (define permute
   (lambda (n r)
-    (/ (fact n) (fact (- n r)))))
+    (/ (factorial n) (factorial (- n r)))))
 
 ;(if (not (= 1 (permute 0 0))) (error "assert 0 0 failed"))
 ;(if (not (= 120 (permute 5 5))) (error "assert failed"))
